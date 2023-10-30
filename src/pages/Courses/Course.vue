5029c37b8de7d8c2f4de8d0d186ab6120a9a5a65
ezFixBug <tdsstoredau@gmail.com>
2023-10-30 21:11:57 +0700

    add course detail page

--- src/pages/Courses/Course.vue
+++ src/pages/Courses/Course.vue
@@ -1,17 +1,17 @@
 <template>
-  <Card hoverable class='mb-4'>
+  <Card hoverable class='mb-4' @click="goToDetailCourse">
+    <router-link :to="{ name: 'detail-course', params: { id: 1 } }" class="text-secondary">
+    </router-link>
     <template #cover>
-      <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style="max-height: 247px; max-width: 370px;" />
+      <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        style="max-height: 247px; max-width: 370px;" />
     </template>
     <div class="course-categories">
       <h6 class="ribbon ribbon-blue-bg fs-14 mb-3 mr-2" v-for="category in course.category" :key="category.id">{{
         category.name }}</h6>
     </div>
     <h4>{{ course.name }}</h4>
-    <p>
-      <router-link :to="{ name: 'detail-course', params: { id: 1 } }" class="text-secondary">{{ course.instructor
-      }}</router-link>
-    </p>
+    <p>{{ course.instructor }}</p>
     <div class="d-flex justify-content-between align-items-center">
       <p>{{ course.price }}</p>
       <div class="icon-element icon-element-sm shadow-sm cursor-pointer btn-add-wish">
@@ -22,6 +22,12 @@
 </template>
 <script>
 export default {
-  props: ['course']
+  props: ['course'],
+
+  methods: {
+    goToDetailCourse() {
+      this.$router.push({ name: 'detail-course', params: { id: this.course.id } });
+    }
+  },
 }
 </script>
